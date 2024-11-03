3687a83a4afb7d4126c85018ead5c02c2e4b6d23
DESKTOP-DE93GD1\seyko <seykota1000@gmail.com>
2024-11-03 10:09:32 +0900

    1. 스크립트 entry point 추가 2. 스네이크 케이스로 변수명 변경 3. private들 _ 붙임

--- define_maker.py
+++ define_maker.py
@@ -22,19 +22,21 @@ def progress_bar(target, current, total, bar_length=20):
 
 class DefineAdder:
     def __init__(self, project_name, url, issue_number, email, api_token, define_files):
-        self.define_files = define_files
-        self.issue_number = issue_number
-        self.url = f"{url}/{issue_id}"
+        self._define_files = define_files
+        self._issue_number = issue_number
+        self._url = f"{url}/{issue_number}"
+        self._email = email
+        self._api_token = api_token
 
-    def addDefineToDefineFiles(project_name, module_name):
-        for file_path in self.define_files:
+    def add_define_to_define_files(self, project_name, module_name):
+        for file_path in self._define_files:
             with open(file_path, 'a') as file:
-                file.write(f"### {self.url}/{project_name}-{self.issue_number}\n")
-                file.write(f"### {self.getSummaryFromJira()}\n")
+                file.write(f"### {self._url}/{project_name}-{self._issue_number}\n")
+                file.write(f"### {self.get_summary_from_jira()}\n")
                 file.write(f"#{module_name}\n")
 
-    def getSummaryFromJira():
-        credentials = f"{self.email}:{self.api_token}"
+    def get_summary_from_jira(self):
+        credentials = f"{self._email}:{self._api_token}"
         encoded_credentials = base64.b64encode(credentials.encode("utf-8")).decode("utf-8")
 
         headers = {
@@ -42,7 +44,7 @@ class DefineAdder:
             "Content-Type": "application/json"
         }
 
-        request = urllib.request.Request(self.url, headers=headers)
+        request = urllib.request.Request(self._url, headers=headers)
 
         summary = ""
         try:
@@ -58,131 +60,137 @@ class DefineAdder:
             print(f"URL Error: {e.reason}")
         return summary
 
+
 class ModuleMaker:
     def __init__(self, user_modified_file, original_file):
-        self.user_modified_file = user_modified_file
-        self.original_file = original_file
-        self.previous_chunk_end_line = 0
-        self.addition_parts = []
-        self.deletion_parts = []
-        self.result = []
+        self._user_modified_file = user_modified_file
+        self._original_file = original_file
+        self._previous_chunk_end_line = 0
+        self._addition_parts = []
+        self._deletion_parts = []
+        self._result = []
 
-    def is_skip_line(self, line):
+    def _is_skip_line(self, line):
         return line.startswith('---') or line.startswith('+++')
 
-    def is_diff_chunk_start_line(self, line):
+    def _is_diff_chunk_start_line(self, line):
         return line.startswith('@@')
 
-    def write_lines_after_previous_chunk_until(self, current_chunk_start_line :int):
-        with open(self.original_file.name, 'r') as infile:
+    def _write_lines_after_previous_chunk_until(self, current_chunk_start_line):
+        with open(self._original_file, 'r') as infile:
             for current_line, line_content in enumerate(infile, start=1):
-                if current_line <= self.previous_chunk_end_line:
+                if current_line <= self._previous_chunk_end_line:
                     continue
                 if current_line > current_chunk_start_line:
                     return
-                self.result.append(line_content)
-    
-    def write_ifdef_block(self):
-            self.result.append(f"#ifdef {moduleName}\n")
-            self.result.append(''.join(self.addition_parts))
-            self.result.append(f"#endif // {moduleName}\n")
+                self._result.append(line_content)
 
-    def write_ifndef_block(self):
-            self.result.append(f"#ifndef {moduleName}\n")
-            self.result.append(''.join(self.deletion_parts))
-            self.result.append(f"#endif // {moduleName}\n")
+    def _write_ifdef_block(self, module_name):
+        self._result.append(f"#ifdef {module_name}\n")
+        self._result.append(''.join(self._addition_parts))
+        self._result.append(f"#endif // {module_name}\n")
 
-    def write_ifdef_else_block(self):
-        self.result.append(f"#ifdef {moduleName}\n")
-        self.result.append(''.join(self.addition_parts))
-        self.result.append(f"#else // {moduleName}\n")
-        self.result.append(''.join(self.deletion_parts))
-        self.result.append(f"#endif // {moduleName}\n")  
-         
-    def write_diff_with_module(self):
-        if self.deletion_parts and self.addition_parts:
-            self.write_ifdef_else_block()
+    def _write_ifndef_block(self, module_name):
+        self._result.append(f"#ifndef {module_name}\n")
+        self._result.append(''.join(self._deletion_parts))
+        self._result.append(f"#endif // {module_name}\n")
 
-        elif len(self.addition_parts) != 0:
-            self.write_ifdef_block()
-        
-        elif len(self.deletion_parts) != 0:
-            self.write_ifndef_block()
-        
-        self.clear_diff_buffers()
+    def _write_ifdef_else_block(self, module_name):
+        self._result.append(f"#ifdef {module_name}\n")
+        self._result.append(''.join(self._addition_parts))
+        self._result.append(f"#else // {module_name}\n")
+        self._result.append(''.join(self._deletion_parts))
+        self._result.append(f"#endif // {module_name}\n")
 
-    def clear_diff_buffers(self):
-        self.addition_parts = []
-        self.deletion_parts = []
+    def _write_diff_with_module(self, module_name):
+        if self._deletion_parts and self._addition_parts:
+            self._write_ifdef_else_block(module_name)
 
-    def is_addtion_part(self, line):
+        elif len(self._addition_parts) != 0:
+            self._write_ifdef_block(module_name)
+
+        elif len(self._deletion_parts) != 0:
+            self._write_ifndef_block(module_name)
+
+        self._clear_diff_buffers()
+
+    def _clear_diff_buffers(self):
+        self._addition_parts = []
+        self._deletion_parts = []
+
+    def _is_addition_part(self, line):
         return line.startswith('+') and not line.startswith('+++')
 
-    def is_deletion_part(self, line):
+    def _is_deletion_part(self, line):
         return line.startswith('-') and not line.startswith('---')
 
-    def modify_file_with_module(self):
-        for line in unified_diff(self.original_file.lines, self.user_modified_file.lines, fromfile=self.original_file.name, tofile=self.user_modified_file.name, lineterm=''):
-            ("line_count", original_file.end_of_file)            
-            if self.is_skip_line(line):
+    def modify_file_with_module(self, module_name):
+        with open(self._original_file, 'r') as original, open(self._user_modified_file, 'r') as modified:
+            original_lines = original.readlines()
+            modified_lines = modified.readlines()
+
+        for line in unified_diff(original_lines, modified_lines, fromfile=self._original_file,
+                                 tofile=self._user_modified_file, lineterm=''):
+            if self._is_skip_line(line):
                 continue
-            
-            if self.is_diff_chunk_start_line(line):
+
+            if self._is_diff_chunk_start_line(line):
                 match = re.search(r"@@ -(\d+).*? @@", line)
                 if match:
                     current_chunk_begin_line = int(match.group(1))
-            
+
                 match = re.search(r"@@ -\d+,(\d+).*? @@", line)
                 if match:
                     current_chunk_length = int(match.group(1))
                 else:
                     current_chunk_length = 1
-                    
-                self.write_lines_after_previous_chunk_until(current_chunk_begin_line - 1)
-                self.previous_chunk_end_line = current_chunk_begin_line + (current_chunk_length - 1)
 
-                self.write_diff_with_module()
+                self._write_lines_after_previous_chunk_until(current_chunk_begin_line - 1)
+                self._previous_chunk_end_line = current_chunk_begin_line + (current_chunk_length - 1)
 
-            elif self.is_addtion_part(line):
-                self.addition_parts.append(line[1:])
+                self._write_diff_with_module(module_name)
 
-            elif self.is_deletion_part(line):
-                self.deletion_parts.append(line[1:])
-            
+            elif self._is_addition_part(line):
+                self._addition_parts.append(line[1:])
+
+            elif self._is_deletion_part(line):
+                self._deletion_parts.append(line[1:])
+
             else:
-                self.write_diff_with_module()
-                self.result.append(line[1:])
-        
-        self.write_diff_with_module()
-        self.write_lines_after_previous_chunk_until(self.original_file.end_of_file)
+                self._write_diff_with_module(module_name)
+                self._result.append(line[1:])
 
-        with open(self.user_modified_file.name, 'w') as f:
-            f.writelines(str(line) for line in self.result)
+        self._write_diff_with_module(module_name)
+        self._write_lines_after_previous_chunk_until(len(original_lines))
 
-
-if len(sys.argv) < 3:
-    print("Usage: python3 define_maker.py MODULE_NAME FILENAME")
-    sys.exit(1)
-
-moduleName = sys.argv[1]
-file_names = sys.argv[2:]
-file_count = len(file_names)
-
-git = Git()
-file_reader = FileReader()
-
-print(f"* start QuickDefine")
-for idx, target_file_name in enumerate(file_names):
-    progress_bar(target_file_name, idx + 1, file_count)
-
-    user_modified_file_name = target_file_name
-    git.make_file_from_last_commit(f"{target_file_name}.orig", target_file_name)
-
-    user_modified_file = file_reader.read_file(user_modified_file_name)       
-    original_file = file_reader.read_file(f"{target_file_name}.orig")       
+        with open(self._user_modified_file, 'w') as f:
+            f.writelines(str(line) for line in self._result)
 
 
-    moduleMaker = ModuleMaker(user_modified_file, original_file)
-    moduleMaker.modify_file_with_module()
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Usage: python3 define_maker.py MODULE_NAME FILENAME")
+        sys.exit(1)
 
-    os.remove(f"{target_file_name}.orig")
+    moduleName = sys.argv[1]
+    file_names = sys.argv[2:]
+    file_count = len(file_names)
+
+    git = Git()
+    file_reader = FileReader()
+
+    print(f"* start QuickDefine")
+    for idx, target_file_name in enumerate(file_names):
+        progress_bar(target_file_name, idx + 1, file_count)
+
+        user_modified_file_name = target_file_name
+        git.make_file_from_last_commit(f"{target_file_name}.orig", target_file_name)
+
+        user_modified_file = file_reader.read_file(user_modified_file_name)
+        original_file = file_reader.read_file(f"{target_file_name}.orig")
+
+
+        moduleMaker = ModuleMaker(user_modified_file, original_file)
+        moduleMaker.modify_file_with_module()
+
+        os.remove(f"{target_file_name}.orig")
